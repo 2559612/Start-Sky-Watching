489ce3b06b5d6f5a0fcb1858635265ffba290592
Nusaiba <p2559612@my365.dmu.ac.uk>
2024-02-19 22:10:07 +0000

    Styled the home page

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,24 @@ menuToggler.addEventListener('click', ev => {
 	menu.classList.toggle('open');
 	menuToggler.textContent = menuToggler.textContent === "×" ? "≡" : "×";
 });
+
+const slideshow = document.querySelector('.slideshow');
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+let currentSlide = 0;
+
+prevButton.addEventListener('click', () => {
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  slideshow.style.transform = `translateX(-${currentSlide * 100}%)`;
+});
+
+nextButton.addEventListener('click', () => {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  slideshow.style.transform = `translateX(-${currentSlide * 100}%)`;
+});
