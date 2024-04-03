0114f3dd76d9811bffad9e5a446bb9ec738a56bf
Nusaiba <p2559612@my365.dmu.ac.uk>
2024-04-04 01:43:19 +0300

    Automatic Slideshow Not Rendering, trying Multiple Slideshows on Multiple Pages.

--- js/scripts.js
+++ js/scripts.js
@@ -4,104 +4,66 @@ menuToggler.addEventListener('click', ev => {
 	menuToggler.textContent = menuToggler.textContent === "×" ? "≡" : "×";
 });
 
-const slideshow31 = document.querySelector('.slideshow31');
-const slideshow32 = document.querySelector('.slideshow32');
-// const slides = document.querySelector('.slide');
-const prev31 = document.querySelector('.prev31');
-const next31 = document.querySelector('.next31');
-const prev32 = document.querySelector('.prev32');
-const next32 = document.querySelector('.next32');
-
-//let curSlide = 0;
-
-// prev.addEventListener('click', () => {
-//   curSlide--;
-//   if (curSlide < 0) {
-//     curSlide = slides.length - 1;
-//   }
-//   slideshow1.style.transform = `translateX(-${curSlide * 100}%)`;
-// });
-
-// nextButton.addEventListener('click', () => {
-//   curSlide++;
-//   if (curSlide >= slides.length) {
-//     curSlide = 0;
-//   }
-//   slideshow1.style.transform = `translateX(-${curSlide * 100}%)`;
-// });
-
-// beginning of the slideshow1 code
-
-// let slideIndex = 1;
-// let slideshowName = "mySlides";
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName(slideshowName);
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "non";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
+const slideshow_2_1 = document.querySelector('.slideshow_2_1');
+const slideshow_2_2 = document.querySelector('.slideshow_2_2');
+const slideshow_3_1 = document.querySelector('.slideshow_3_1');
+const prev_2_1 = document.querySelector('.prev_2_1');
+const next_2_1 = document.querySelector('.next_2_1');
+const prev_2_2 = document.querySelector('.prev_2_2');
+const next_2_2 = document.querySelector('.next_2_2');
+const prev_3_1 = document.querySelector('.prev_3_1');
+const next_3_1 = document.querySelector('.next_3_1');
 
 
-
-// let slideIndex = 1;
-// showSlides1(slideIndex);
-// showSlides2(slideIndex)
-
-
-
-let slideIndex = [1,1];
+let slideIndex = [1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides31", "mySlides32"]
+let slideId = ["mySlides_2_1", "mySlides_2_2", "mySlides_3_1"];
 showSlides(1, 0);
 showSlides(1, 1);
-
-prev31.addEventListener('click', () => {
-  plusSlides(-1, 0)
-});
-
-next31.addEventListener('click', () => {
-  plusSlides(1, 0)
-});
-
-prev32.addEventListener('click', () => {
-  plusSlides(-1, 1)
-});
-
-next32.addEventListener('click', () => {
-  plusSlides(1, 1)
-});
-
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
+showSlides(1, 2);
 
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
+  if (n > x.length) {slideIndex[no] = 1} 
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
   x[slideIndex[no]-1].style.display = "block";
 }
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+prev_2_1.addEventListener('click', () => {
+  plusSlides(-1, 0)
+});
+
+next_2_1.addEventListener('click', () => {
+  plusSlides(1, 0)
+});
+
+prev_2_2.addEventListener('click', () => {
+  plusSlides(-1, 1)
+});
+
+next_2_2.addEventListener('click', () => {
+  plusSlides(1, 1)
+});
+
+prev_3_1.addEventListener('click', () => {
+  plusSlides(-1, 2)
+});
+
+next_3_1.addEventListener('click', () => {
+  plusSlides(1, 2)
+});
+
+
+
+
+
+
+
